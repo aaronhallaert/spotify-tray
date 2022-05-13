934797a9eaacae82bb207d2d176504f5832a0830
Yannick Maes <yannick.maes@nipro-group.com>
2022-05-14 00:38:14 +0200

    run auto format and fix warnings

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
-    "strconv"
 
 	"github.com/getlantern/systray"
 	"github.com/skratchdot/open-golang/open"
 )
 
 var scriptsPath = "./scripts/"
+
 // var scriptsPath = "../Resources/"
 
 func main() {
@@ -22,110 +23,106 @@ func main() {
 }
 
 type SpotifyStatus struct {
-    track string
-    artist string
-    album string
-    status string
-    duration float64
-    position float64
-    progress int
+	track    string
+	artist   string
+	album    string
+	status   string
+	duration float64
+	position float64
+	progress int
 }
 
 func (s *SpotifyStatus) Format() string {
-    return fmt.Sprintf("%d%% %s  %s - %s", s.progress, s.status, trimString(s.artist, 20), trimString(s.track, 20))
+	return fmt.Sprintf("%d%% %s  %s - %s", s.progress, s.status, trimString(s.artist, 20), trimString(s.track, 20))
 }
 
 func trimString(s string, maxLength int) string {
-    if len(s) > maxLength {
-        trimmed := s[:maxLength] + "..."
-        return trimmed
-    }
-    return s
+	if len(s) > maxLength {
+		trimmed := s[:maxLength] + "..."
+		return trimmed
+	}
+	return s
 }
 
-func fetchSpotifyStatus() SpotifyStatus { 
-    nTrack, err := exec.Command("/bin/sh", scriptsPath+"track.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    track := strings.TrimSuffix(string(nTrack), "\n")
+func fetchSpotifyStatus() SpotifyStatus {
+	nTrack, err := exec.Command("/bin/sh", scriptsPath+"track.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	track := strings.TrimSuffix(string(nTrack), "\n")
 
-    nArtist, err := exec.Command("/bin/sh", scriptsPath+"artist.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    artist := strings.TrimSuffix(string(nArtist), "\n")
+	nArtist, err := exec.Command("/bin/sh", scriptsPath+"artist.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	artist := strings.TrimSuffix(string(nArtist), "\n")
 
-    nStatus, err := exec.Command("/bin/sh", scriptsPath+"status.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    status := strings.TrimSuffix(string(nStatus), "\n")
+	nStatus, err := exec.Command("/bin/sh", scriptsPath+"status.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	status := strings.TrimSuffix(string(nStatus), "\n")
 
-    nAlbum, err := exec.Command("/bin/sh", scriptsPath+"album.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    album := strings.TrimSuffix(string(nAlbum), "\n")
+	nAlbum, err := exec.Command("/bin/sh", scriptsPath+"album.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	album := strings.TrimSuffix(string(nAlbum), "\n")
 
-    nDuration, err := exec.Command("/bin/sh", scriptsPath+"duration.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    duration := strings.TrimSuffix(string(nDuration), "\n")
-    durationFloat, err := strconv.ParseFloat(duration, 64)
-    durationFloat = durationFloat / 1000
+	nDuration, err := exec.Command("/bin/sh", scriptsPath+"duration.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	duration := strings.TrimSuffix(string(nDuration), "\n")
+	durationFloat, _ := strconv.ParseFloat(duration, 64)
+	durationFloat = durationFloat / 1000
 
+	nPosition, err := exec.Command("/bin/sh", scriptsPath+"position.sh").Output()
+	if err != nil {
+		fmt.Printf("error %s", err)
+	}
+	position := strings.TrimSuffix(string(nPosition), "\n")
+	position = strings.ReplaceAll(position, ",", ".")
+	positionFloat, _ := strconv.ParseFloat(position, 64)
+	progress := int((positionFloat / durationFloat) * 100)
 
-    nPosition, err := exec.Command("/bin/sh", scriptsPath+"position.sh").Output()
-    if err != nil {
-        fmt.Printf("error %s", err)
-    }
-    position := strings.TrimSuffix(string(nPosition), "\n")
-    position = strings.ReplaceAll(position, ",", ".")
-    positionFloat, err := strconv.ParseFloat(position, 64)
-    progress := int((positionFloat / durationFloat) * 100)
-
-    return SpotifyStatus {
-        track: track,
-        artist: artist,
-        album: album,
-        status: status,
-        duration: durationFloat,
-        position: positionFloat,
-        progress: progress,
-    }
+	return SpotifyStatus{
+		track:    track,
+		artist:   artist,
+		album:    album,
+		status:   status,
+		duration: durationFloat,
+		position: positionFloat,
+		progress: progress,
+	}
 }
 
 func onReady() {
 	systray.SetTitle("Loading...")
-    mLyrics := systray.AddMenuItem("Lyrics", "Search lyrics")
+	mLyrics := systray.AddMenuItem("Lyrics", "Search lyrics")
 	mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
 
-    currentSpotifyStatus := fetchSpotifyStatus()
+	currentSpotifyStatus := fetchSpotifyStatus()
 
-    systray.AddSeparator()
-    go func() {
-        <-mQuitOrig.ClickedCh
-        fmt.Println("Requesting quit")
-        systray.Quit()
-        fmt.Println("Finished quitting")
-    }()
-
+	systray.AddSeparator()
 	go func() {
-        select {
-        case <-mLyrics.ClickedCh:
-            open.Run("https://www.google.be/search?q=" + currentSpotifyStatus.track + " lyrics")
-            return
-        }
+		<-mQuitOrig.ClickedCh
+		fmt.Println("Requesting quit")
+		systray.Quit()
+		fmt.Println("Finished quitting")
 	}()
 
 	go func() {
-        for {
-            currentSpotifyStatus = fetchSpotifyStatus()
-            message := currentSpotifyStatus.Format()
-            systray.SetTitle(message)
-            time.Sleep(time.Millisecond * 300)
-        }
+		<-mLyrics.ClickedCh
+		open.Run("https://www.google.be/search?q=" + currentSpotifyStatus.track + " lyrics")
+	}()
+
+	go func() {
+		for {
+			currentSpotifyStatus = fetchSpotifyStatus()
+			message := currentSpotifyStatus.Format()
+			systray.SetTitle(message)
+			time.Sleep(time.Millisecond * 300)
+		}
 	}()
 }
